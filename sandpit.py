ec7a13f74a66f8dad8dba53ee5aa4cde9c739317
Refael Asher Davis <rafi.davis14@gmail.com>
2025-07-24 22:30:15 +0100

    Created an implementation for the maze in which the hero operates.

--- sandpit.py
+++ sandpit.py
@@ -1,22 +1,9 @@
-from game_model import *
-from game_interface import *
-import pytest
-import re
-from tabulate import tabulate
-import numpy as np
+def create_grid(rows, cols):
+        grid = [[None for _ in range(cols)] for _ in range(rows)]
 
-def visualise_game():
-        """Provide the visual element of the game. 
-        The game is set in a 5x5 grid. The hero begins in the top-left and the game only successfully 
-        end if the hero makes it to the bottom right.
-        """
-        """
-        headers = [" ", " ", " ", " ", " "]
-        table = [[" ", " ", " ", " ", " "], [" ", " ", " ", " ", " "], [" ", " ", " ", " ", " "], [" ", " ", " ", " ", " "]]
-        
-        print(tabulate(table, headers, tablefmt = "grid"))
-        """
-        a = np.zeros(5)
-        print(a)
+def print_grid(grid):
+        for row in grid:
+                print(" | ".join(type(cell).__name__[0] if cell else " " for cell in row))
 
-visualise_game()
+grid = create_grid(2,2)
+print_grid(grid)
